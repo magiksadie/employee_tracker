12c92fd9603bd337679c374bf8e30139075a3c7a
sadie.harty <82483760+magiksadie@users.noreply.github.com>
2022-05-22 20:26:40 -0500

    initial server attempt

--- server.js
+++ server.js
@@ -1,18 +1,62 @@
-// get the client
-const mysql = require('mysql2');
+const connection = require('./db/connection');
+const inquirer = require('inquirer');
+const cTable = require('console.table');
 
-// create the connection to database
-const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    database: 'test'
+//Connect to DB
+connection.connect(err => {
+    if (err) throw err;
+    console.log(`
+    Welcome to your Employee Tracker!`)
 });
 
-// simple query
-connection.query(
-  'SELECT * FROM `table` WHERE `name` = "Page" AND `age` > 45',
-    function(err, results, fields) {
-        console.log(results); // results contains rows returned by server
-        console.log(fields); // fields contains extra meta data about results, if available
-    }
-);
+//Begin the program
+function promptQuestions() {
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'menu',
+            message: 'What would you like to do?',
+            choices: ['View All Employees', 'View All Employees By Department', 'View All Employees By Manager', 'Add Employee', 'Add Department', 'Add Role', 'Update Employee Role', 'Update Employee Manager', 'Remove Employee', 'Remove Department', 'Remove Role', 'Exit'],
+        }
+    ]).then(answers => {
+        const { menu } = answers;
+        switch (menu) {
+            case 'View All Employees':
+                viewAllEmployees();
+                break;
+            case 'View All Employees By Department':
+                viewAllEmployeesByDepartment();
+                break;
+            case 'View All Employees By Manager':
+                viewAllEmployeesByManager();
+                break;
+            case 'Add Employee':
+                addEmployee();
+                break;
+            case 'Add Department':
+                addDepartment();
+                break;
+            case 'Add Role':
+                addRole();
+                break;
+            case 'Update Employee Role':
+                updateEmployeeRole();
+                break;
+            case 'Update Employee Manager':
+                updateEmployeeManager();
+                break;
+            case 'Remove Employee':
+                removeEmployee();
+                break;
+            case 'Remove Department':
+                removeDepartment();
+                break;
+            case 'Remove Role':
+                removeRole();
+                break;
+            case 'Exit':
+                connection.end();
+                break;
+        }
+    });
+}
